85c17a849236343ed75f4d48365487c95c68a3e8
vajakath <42489918+vajakath@users.noreply.github.com>
2020-12-20 10:12:18 +0530

    Add files via upload

---
+++ kamal.js
@@ -0,0 +1,27 @@
+function gencat() {
+    var img = document.createElement("img");
+    img.src = "download.jpg";
+    var div = document.getElementById('cat');
+    div.appendChild(img);
+}
+
+
+
+
+//  function vaja(b) {
+//      var a = prompt("Calulate age by days");
+//      console.log(b * a);
+//      var f = b * a;
+//      var k = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+//      console.log(k)
+//      var l = { name: "vaja", age: 21 };
+//      console.log(l.name)
+//      var h1 = document.createElement('h1');
+//      var t = document.createTextNode("my days are" + " " + f);
+//      h1.append(t);
+//      document.getElementById('result').appendChild(h1);
+//  }
+
+//  function reset() {
+//      document.getElementById('result').remove();
+//  }
